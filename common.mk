7b700b604ad73e234cf316ace9f7d6f660d8e1c7
Gary Bisson <gary.bisson@boundarydevices.com>
2021-02-16 15:22:43 +0100

    kernel: fix build issues
    - add missing build packages required to build
    - remove adopt-info as otherwise missing parse-info
    - fix common.mk now that snapcraft.yaml location has changed
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- common.mk
+++ common.mk
@@ -4,5 +4,5 @@ GADGET_DIR := $(OUTPUT_DIR)/gadget
 GADGET_VERSION := `grep version $(GADGET_DIR)/meta/snap.yaml | awk '{print $$2}'`
 GADGET_SNAP := nitrogen-gadget_$(GADGET_VERSION)_armhf.snap
 KERNEL_DIR := $(OUTPUT_DIR)/kernel
-KERNEL_VERSION := `cat $(KERNEL_DIR)/snapcraft.yaml | grep version: | awk '{print $$2}'`
+KERNEL_VERSION := `grep "version:" $(KERNEL_DIR)/snap/snapcraft.yaml | awk '{print $$2}'`
 KERNEL_SNAP := nitrogen-kernel_$(KERNEL_VERSION)_armhf.snap
