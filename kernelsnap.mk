905d117656491d70cbdb10675edcfba31ecbd125
Gary Bisson <gary.bisson@boundarydevices.com>
2021-02-16 14:52:39 +0100

    kernel: adjust snapcraft files layout
    - move snapcraft.yaml to snap folder
    - add missing plugins files [1]
    
    [1] https://github.com/kubiko/snapcraft-kernel-plugin
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- kernelsnap.mk
+++ kernelsnap.mk
@@ -27,7 +27,7 @@ distclean: clean
 
 build: kernel_src qcacld_src firmware
 	unset ARCH CROSS_COMPILE
-	cd $(KERNEL_DIR); snapcraft --target-arch armhf snap
+	cd $(KERNEL_DIR); snapcraft --destructive-mode --target-arch=armhf
 	mv $(KERNEL_DIR)/$(KERNEL_SNAP) $(OUTPUT_DIR)
 
 kernel_src:
